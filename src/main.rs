ebac8552935d688fdc83d977f4a19c48ed19c0a4
Harmelodic <matt@harmelodic.com>
2021-07-20 19:02:33 +0200

    Pseudo code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
+
+    // Gets files in directory
+
+    // For file name in files
+        // Parse file name to see if it has a date
+
+        // if it has a date
+            // Construct new file name
+            // Log out: old_file_name --to--> new_file_name
+            // Change the file name to the new format
+        // else
+            // Log out that we haven't changed the date for this file
+        // end
+    // end
 }
